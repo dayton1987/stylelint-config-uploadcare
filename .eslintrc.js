3913362ea8653360dca517dbbf449d8493366f64
Alexander Kurganov <me@akurganow.ru>
2016-10-24 19:30:42 +0300

    Fix js files

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  extends: 'uploadcare',
+}
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,6 @@
-import config from "../"
-import stylelint from "stylelint"
-import test from "ava"
+import config from '../'
+import stylelint from 'stylelint'
+import test from 'ava'
 
 const validCss = (
 `@import url(x.css);
@@ -83,29 +83,31 @@ const invalidCss = (
 
 `)
 
-test("no warnings with valid css", t => {
-  return stylelint.lint({
+test('no warnings with valid css', test =>
+  stylelint.lint({
     code: validCss,
     config: config,
   })
   .then(data => {
-    const { errored, results } = data
-    const { warnings } = results[0]
-    t.falsy(errored, "no errored")
-    t.is(warnings.length, 0, "flags no warnings")
-  })
-})
+    const {errored, results} = data
+    const {warnings} = results[0]
 
-test("a warning with invalid css", t => {
-  return stylelint.lint({
+    test.falsy(errored, 'no errored')
+    test.is(warnings.length, 0, 'flags no warnings')
+  })
+)
+
+test('a warning with invalid css', test =>
+  stylelint.lint({
     code: invalidCss,
     config: config,
   })
   .then(data => {
-    const { errored, results } = data
-    const { warnings } = results[0]
-    t.truthy(errored, "errored")
-    t.is(warnings.length, 1, "flags one warning")
-    t.is(warnings[0].text, "Expected a leading zero (number-leading-zero)", "correct warning text")
+    const {errored, results} = data
+    const {warnings} = results[0]
+
+    test.truthy(errored, 'errored')
+    test.is(warnings.length, 1, 'flags one warning')
+    test.is(warnings[0].text, 'Expected a leading zero (number-leading-zero)', 'correct warning text')
   })
-})
+)
